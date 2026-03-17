7397bfdd8bd7f4d23a5cfaf6807638e80ecc2eba
JoshuaE-C <zwnx0724@leeds.ac.uk>
2026-03-17 05:43:10 +0000

    T2 done

--- worksheet/task_2/cinema.py
+++ worksheet/task_2/cinema.py
@@ -9,7 +9,6 @@ Please do not add any additional code underneath these functions.
 
 import sqlite3
 
-
 def customer_tickets(conn, customer_id):
     """
     Return a list of tuples:
@@ -18,7 +17,9 @@ def customer_tickets(conn, customer_id):
     Include only tickets purchased by the given customer_id.
     Order results by film title alphabetically.
     """
-    pass
+    query = f"SELECT title, screen, price FROM films INNER JOIN screenings ON films.film_id=screenings.film_id INNER JOIN tickets ON screenings.screening_id=tickets.screening_id INNER JOIN customers ON customers.customer_id=tickets.customer_id WHERE customers.customer_id={customer_id} ORDER BY title ASC;"
+    cur = conn.cursor()
+    return cur.execute(query).fetchall()
 
 
 def screening_sales(conn):
@@ -29,7 +30,19 @@ def screening_sales(conn):
     Include all screenings, even if tickets_sold is 0.
     Order results by tickets_sold descending.
     """
-    pass
+
+    query = """
+    SELECT screenings.screening_id, title, COUNT(ticket_id) AS tickets_sold 
+    FROM screenings 
+    INNER JOIN films 
+    ON screenings.film_id=films.film_id 
+    LEFT JOIN tickets 
+    ON screenings.screening_id=tickets.screening_id 
+    GROUP BY screenings.screening_id 
+    ORDER BY tickets_sold DESC;"""
+
+    cur = conn.cursor()
+    return cur.execute(query).fetchall()
 
 
 def top_customers_by_spend(conn, limit):
@@ -42,4 +55,17 @@ def top_customers_by_spend(conn, limit):
     Order by total_spent descending.
     Limit the number of rows returned to `limit`.
     """
-    pass
+
+    query = f"""
+    SELECT customer_name, SUM(price) total_spent
+    FROM tickets
+    INNER JOIN customers 
+    ON customers.customer_id=tickets.customer_id 
+    GROUP BY customers.customer_id
+    ORDER BY total_spent DESC 
+    LIMIT {limit}
+    ;"""
+
+    cur = conn.cursor()
+    return cur.execute(query).fetchall()
+    
